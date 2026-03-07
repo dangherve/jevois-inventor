6fac6faeeb8a2cf4224e4b94e31e5f13027ddd2e
dangherve <dangherve@gmail.com>
2026-03-07 01:24:21 -0500

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/jevois/jevois-inventor.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/jevois-inventor.git
+git push --force --set-upstream origin master
